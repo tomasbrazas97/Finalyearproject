2d29f6b6ad9fef0fab23491393ecf7082f9f188a
tomasbrazas97 <tomasbrazas@gmail.com>
2020-05-02 17:40:34 +0100

    Auth done

--- client/src/app/authentication.service.ts
+++ client/src/app/authentication.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core'
-import {HttpClient} from "@angular/common/http"
+import {HttpClient} from '@angular/common/http'
 import {Observable, of} from 'rxjs'
 import {map} from 'rxjs/operators'
 import {Router} from '@angular/router'

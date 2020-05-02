8fec4634e18865378d89af9f95abaa2d7ccb0d50
tomasbrazas97 <tomasbrazas@gmail.com>
2020-05-02 19:03:08 +0100

    model typo fix

--- models/User.js
+++ models/User.js
@@ -1,25 +1,26 @@
-const mongoose = require("mongoose")
+const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
-const UserSchema = new Schema ({
-    first_name: {
-        type: String
-    },
-    last_name: {
-        type: String
-    },
-    email: {
-        type: String,
-        require: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    date: {
-        type: Date,
-        default: Date.now
-    }
+// Create Schema
+const UserSchema = new Schema({
+  first_name: {
+    type: String
+  },
+  last_name: {
+    type: String
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
 })
 
 module.exports = User = mongoose.model('users', UserSchema)

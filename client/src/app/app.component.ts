45a2f042dda9c94e7f2ce78b384c1044186fa358
tomasbrazas97 <tomasbrazas@gmail.com>
2020-05-04 18:46:03 +0100

    fix
    Appearing home page instead of nav bar

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -8,7 +8,4 @@ import {AuthenticationService} from './authentication.service'
 })
 export class AppComponent {
   constructor(public auth: AuthenticationService) {}
-
-  lat = 40.730610;
-  lng = -73.935242;
 }

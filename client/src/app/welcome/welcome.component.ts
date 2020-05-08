78a0542461cd25c784f4b20af432158ac8f3288b
tomasbrazas97 <tomasbrazas@gmail.com>
2020-05-08 17:59:21 +0100

    Welcome page css

--- client/src/app/welcome/welcome.component.ts
+++ client/src/app/welcome/welcome.component.ts
@@ -1,8 +1,17 @@
 import { Component } from '@angular/core'
+import { Router } from '@angular/router'
 
 
 @Component({
-  templateUrl: './welcome.component.html'
+  templateUrl: './welcome.component.html',
+  styleUrls: ['./welcome.component.css'],
 })
 
-export class WelcomeComponent{}
+export class WelcomeComponent{
+    constructor(private router: Router) { }
+
+    toRegister() {
+        this.router.navigateByUrl('/register');
+    }
+
+}
